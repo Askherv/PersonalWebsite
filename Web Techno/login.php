a54c7c2283fce01384d06b71a52f2b00cf0cf208
Nihad Asgarov <askhervn@gmail.com>
2024-05-17 19:37:13 +0300

    Personal Website project

--- Web Techno/login.php
+++ Web Techno/login.php
@@ -11,7 +11,7 @@
         if ($username==$user && $password==$pass)
         {
             $_SESSION["password"] = $password;
-            header('location:hakkimda.php');
+            header('location:index.php');
             exit();
         }
         else 
@@ -25,7 +25,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="stylesheet" href="styles/login.css">
     <link rel="shortcut icon" href="assets/hacker.png" type="image/x-icon">
     <title>N.A. | Giriş</title>
